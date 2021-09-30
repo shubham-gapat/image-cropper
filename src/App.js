e34d8498f8b015c43105c06fdfaba2b91ae230a1
shubham-gapat <55310910+shubham-gapat@users.noreply.github.com>
2021-09-30 20:15:24 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 import ImageInput from "./file/ImageInput";
-import Default_User_Pic from "./defaultUserPic.svg"
+import Default_User_Pic from "./defaultUserPic.svg";
 
-
-function App() {
-const[image,setImage]=useState("")
-console.log(image)
+const App = () => {
+  const [image, setImage] = useState("");
   return (
     <div className="col image-input">
-          <ImageInput
-            imageData={image.photo?.src}
-            defaultPic={Default_User_Pic}
-            type="admin"
-            name="photo"
-            label="Add Photo"
-            showPreview
-            onChange={files => setImage(files, "admin")}
-          />
-        </div>
+      <ImageInput
+        imageData={image.photo?.src}
+        defaultPic={Default_User_Pic}
+        type="admin"
+        name="photo"
+        label="Add Photo"
+        showPreview
+        onChange={(files) => setImage(files, "admin")}
+      />
+    </div>
   );
-}
+};
 
 export default App;
-
